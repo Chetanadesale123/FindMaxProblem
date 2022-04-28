21917e6091efdf244dfb2659e0d3fae52e0d233a
59114_Chetana <deslechetna1999@gmail.com>
2022-04-29 00:12:59 +0530

    [Chetana]Added:Find Maximum Float

--- FindMaxProblem/FindMax.cs
+++ FindMaxProblem/FindMax.cs
@@ -26,6 +26,24 @@ namespace FindMaxProblem
                 return third;
             }
         }
+        public float FindMaxFloat(float first, float second, float third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("first is greater" + first);
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("first is greater" + first);
+                return second;
+            }
+            else
+            {
+                Console.WriteLine("Third is greater");
+                return third;
+            }
+        }
     }
 }
 
--- TestMax/UnitTest1.cs
+++ TestMax/UnitTest1.cs
@@ -26,6 +26,27 @@ namespace TestMax
             int actual = max.FindMaxInteger(1, 2, 3);
             Assert.AreEqual(actual, 3);
         }
+        [Test]
+        public void GivenfloatMaxNumInPositionOne_WhenTestMax_ShouldRAETURNMaxNum()
+        {
+            FindMax max = new FindMax();
+            float actual = max.FindMaxFloat(77.9f,18.5f, 3.3f);
+            Assert.AreEqual(actual,77.9f);
+        }
+        [Test]
+        public void GivenfoatMaxNumInPositionTwo_WhenTestMax_ShouldRAETURNMaxNum()
+        {
+            FindMax max = new FindMax();
+            float actual = max.FindMaxFloat(18.5f,77.9f,3.3f);
+            Assert.AreEqual(actual, 77.9f);
+        }
+        [Test]
+        public void GivenfloatMaxNumInPositionThree_WhenTestMax_ShouldRAETURNMaxNum()
+        {
+            FindMax max = new FindMax();
+            float actual = max.FindMaxFloat(3.3f,18.5f,77.9f);
+            Assert.AreEqual(actual, 77.9f);
+        }
 
     }
 }
