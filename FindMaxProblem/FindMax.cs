8106c17b6ca0e05059429bc1c21e4a3d7544e3c0
59114_Chetana <deslechetna1999@gmail.com>
2022-04-29 19:36:19 +0530

    [Chetana]Added: Refactor all the three to one generic method

--- FindMaxProblem/FindMax.cs
+++ FindMaxProblem/FindMax.cs
@@ -6,9 +6,16 @@ using System.Threading.Tasks;
 
 namespace FindMaxProblem
 {
-    public class FindMax
+    public class FindMax<T> where T : IComparable
     {
-        public int FindMaxInteger(int first, int second, int third)
+        public T first, second, third;
+        public FindMax(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public T FindMaximum()
         {
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
             {
@@ -26,42 +33,7 @@ namespace FindMaxProblem
                 return third;
             }
         }
-        public float FindMaxFloat(float first, float second, float third)
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                Console.WriteLine("first is greater" + first);
-                return first;
-            }
-            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                Console.WriteLine("first is greater" + first);
-                return second;
-            }
-            else
-            {
-                Console.WriteLine("Third is greater");
-                return third;
-            }
-        }
-        public string FindMaxString(string first, string second, string third)
-        {
-            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
-            {
-                Console.WriteLine("first is greater" + first);
-                return first;
-            }
-            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
-            {
-                Console.WriteLine("first is greater" + first);
-                return second;
-            }
-            else
-            {
-                Console.WriteLine("Third is greater");
-                return third;
-            }
-        }
+
     }
 }
 
--- TestMax/UnitTest1.cs
+++ TestMax/UnitTest1.cs
@@ -6,67 +6,17 @@ namespace TestMax
     public class Tests
     {
         [Test]
-        public void GivenIntMaxNumInPositionOne_WhenTestMax_ShouldRAETURNMaxNum()
+        public void GivenGenericInput_WhenTestMax_ShouldRerturnMaxNum()
         {
-            FindMax max = new FindMax();
-            int actual = max.FindMaxInteger(3, 2, 1);
+            FindMax<int> max = new FindMax<int>(3, 2, 1);
+            int actual = max.FindMaximum();
             Assert.AreEqual(actual, 3);
-        }
-        [Test]
-        public void GivenIntMaxNumInPositionTwo_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            int actual = max.FindMaxInteger(2, 3, 1);
-            Assert.AreEqual(actual, 3);
-        }
-        [Test]
-        public void GivenIntMaxNumInPositionThree_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            int actual = max.FindMaxInteger(1, 2, 3);
-            Assert.AreEqual(actual, 3);
-        }
-        [Test]
-        public void GivenfloatMaxNumInPositionOne_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            float actual = max.FindMaxFloat(77.9f,18.5f, 3.3f);
-            Assert.AreEqual(actual,77.9f);
-        }
-        [Test]
-        public void GivenfoatMaxNumInPositionTwo_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            float actual = max.FindMaxFloat(18.5f,77.9f,3.3f);
-            Assert.AreEqual(actual, 77.9f);
-        }
-        [Test]
-        public void GivenfloatMaxNumInPositionThree_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            float actual = max.FindMaxFloat(3.3f,18.5f,77.9f);
-            Assert.AreEqual(actual, 77.9f);
-        }
-        [Test]
-        public void GivenStringMaxNumInPositionOne_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            string actual = max.FindMaxString("python","cpp","java");
-            Assert.AreEqual(actual,"python");
-        }
-        [Test]
-        public void GivenStringMaxNumInPositionTwo_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            string actual = max.FindMaxString("cpp","python","java");
-            Assert.AreEqual(actual,"python");
-        }
-        [Test]
-        public void GivenStringMaxNumInPositionThree_WhenTestMax_ShouldRAETURNMaxNum()
-        {
-            FindMax max = new FindMax();
-            string actual = max.FindMaxString("java","cpp","python");
-            Assert.AreEqual(actual, "python");
+            FindMax<float> max1 = new FindMax<float>(77.9f, 18.5f, 3.3f);
+            float actual1 = max1.FindMaximum();
+            Assert.AreEqual(actual1, 77.9f);
+            FindMax<string> max2 = new FindMax<string>("python", "cpp", "java");
+            string actual2 = max2.FindMaximum();
+            Assert.AreEqual(actual2, "python");
         }
     }
 }
